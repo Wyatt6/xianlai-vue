247263771ee4afdd21d7944dd56156a4f2f01093
WyattMac <wyatt6@163.com>
2025-10-02 10:07:12 +0800

    工程初始化

---
+++ src/main.js
@@ -0,0 +1,16 @@
+import ElementPlus from 'element-plus'
+import zhCN from 'element-plus/es/locale/lang/zh-cn'
+import 'element-plus/dist/index.css'
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+
+import App from './App.vue'
+import router from './router'
+
+const app = createApp(App)
+
+app.use(ElementPlus, { locale: zhCN })
+app.use(createPinia())
+app.use(router)
+
+app.mount('#app')
