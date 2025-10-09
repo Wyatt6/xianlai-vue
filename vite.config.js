e8b53fd6366163a13ab639de5eada44da18d81c9
WyattMac <wyatt6@163.com>
2025-10-09 14:32:56 +0800

    修改svg图标搭建配置

--- vite.config.js
+++ vite.config.js
@@ -7,21 +7,7 @@ import { viteAwesomeSvgLoader } from 'vite-awesome-svg-loader'
 // vite配置: https://vite.dev/config/
 // vite-awesome-svg-loader插件配置: https://github.com/matafokka/vite-awesome-svg-loader
 export default defineConfig({
-  plugins: [
-    vue(),
-    viteAwesomeSvgLoader({
-      defaultImport: 'source'
-      // 其他可能用到的配置
-      // A list of files or directories to preserve line width of.
-      // preserveLineWidthList: [/config-demo\/preserve-line-width\//, /config-demo\/all\//],
-      // A list of files or directories to preserve color of
-      // setCurrentColorList: [/config-demo\/set-current-color\//, /config-demo\/all\//],
-      // A list of files to skip while transforming
-      // skipTransformsList: [/skip-transforms\.svg/],
-      // A list of files to skip loading of
-      // skipFilesList: [/skip-loading\.svg/]
-    })
-  ],
+  plugins: [vue(), viteAwesomeSvgLoader({ defaultImport: 'source' })],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
