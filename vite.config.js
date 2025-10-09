b4a83f9ffa3ec5764b5c086836f80e49b9446cf1
WyattMac <wyatt6@163.com>
2025-10-09 14:31:59 +0800

    修改代理访问端口

--- vite.config.js
+++ vite.config.js
@@ -39,7 +39,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:28888/',
+        target: 'http://localhost:30000/',
         changeOrigin: true
       }
     }
