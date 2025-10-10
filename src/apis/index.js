2d331570e6d5d58b29e2c17d96302d525b03d6ab
WyattMac <wyatt6@163.com>
2025-10-10 20:35:15 +0800

    系统接口纳入后台管理

--- src/apis/index.js
+++ src/apis/index.js
@@ -12,42 +12,45 @@
 
 import { defineStore } from 'pinia'
 import { ref } from 'vue'
-import { createAxiosInstance } from './instance'
 import { isEmpty, hasText, notEmpty } from '@/utils/common'
-
-import CommonApis from './modules/common'
-import IamApis from './modules/iam'
-
-const apiList = [...CommonApis, ...IamApis]
+import { createAxiosInstance } from './instance'
 
 export const useApiStore = defineStore('api', () => {
   const request = ref({})
+  const checksum = ref(null)
 
-  function evalData() {
+  /**
+   * 系统接口赋值函数
+   */
+  async function evalData(apiData, checksumData) {
+    checksum.value = checksumData
     const axiosInstance = createAxiosInstance()
-    for (let i = 0; i < apiList.length; i++) {
-      const { callPath, requestMethod, url, description } = apiList[i]
-      const paths = callPath.split('.')
-      let now = request.value
-      for (let j = 0; j < paths.length; j++) {
-        if (j == paths.length - 1) {
-          now[paths[j]] = (requestParams, requestData) => {
-            const config = { method: requestMethod, url: url }
-            if (hasText(description)) config.desc = description
-            if (notEmpty(requestParams)) config.params = requestParams
-            if (notEmpty(requestData)) config.data = requestData
-            return axiosInstance(config)
+    if (notEmpty(apiData)) {
+      apiData.forEach(item => {
+        const { callPath, requestMethod, url, description } = item
+        const paths = callPath.split('.')
+        let now = request.value
+        for (let i = 0; i < paths.length; i++) {
+          if (i == paths.length - 1) {
+            now[paths[i]] = (requestParams, requestData) => {
+              const config = { method: requestMethod, url: url }
+              if (hasText(description)) config.desc = description
+              if (notEmpty(requestParams)) config.params = requestParams
+              if (notEmpty(requestData)) config.data = requestData
+              return axiosInstance(config)
+            }
+          } else {
+            if (isEmpty(now[paths[i]])) now[paths[i]] = {}
+            now = now[paths[i]]
           }
-        } else {
-          if (isEmpty(now[paths[j]])) now[paths[j]] = {}
-          now = now[paths[j]]
         }
-      }
+      })
     }
   }
 
   return {
     request,
+    checksum,
     evalData
   }
 })
