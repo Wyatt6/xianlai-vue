4cd4e6b1477dd76adb2695d59896d93418de74ce
WyattMac <wyatt6@163.com>
2025-10-11 11:55:00 +0800

    路由前置守卫

--- src/router/index.js
+++ src/router/index.js
@@ -1,16 +1,13 @@
-import Routes from './routes'
-import { noLoginRequired, noPermissionRequired } from './white_list'
-import Token from '@/utils/token'
-import { ElMessage } from 'element-plus'
-import { useAppStore } from '@/stores/app'
-import { useAuthorityStore } from '@/stores/authority'
-import routeTable from './routeTable'
-
 import { defineStore } from 'pinia'
 import { ref } from 'vue'
+import { createRouter, createWebHistory } from 'vue-router'
+import { ElMessage } from 'element-plus'
 import { usePathStore } from '@/stores/path'
 import { notEmpty, hasText } from '@/utils/common'
-import { createRouter, createWebHistory } from 'vue-router'
+import Logger from '@/utils/logger'
+import Token from '@/utils/token'
+import { useAppStore } from '@/stores/app'
+import { useAuthorityStore } from '@/stores/authority'
 
 const viewComponents = import.meta.glob('@/views/**/*.vue')
 
@@ -54,6 +51,9 @@ export const useRouterStore = defineStore('router', () => {
     }
   }
 
+  /**
+   * 获取router实例
+   */
   function getRouter() {
     if (notEmpty(router.value)) {
       return router.value
@@ -61,6 +61,56 @@ export const useRouterStore = defineStore('router', () => {
       router.value = createRouter({
         history: createWebHistory(import.meta.env.BASE_URL),
         routes: routes.value
+      })
+      /**
+       * 路由前置守卫：每次路由切换时先执行的操作
+       * @param {*} to   要到哪里去
+       * @param {*} from 从哪里来
+       * @param {*} next 是否要去？
+       */
+      router.value.beforeEach((to, from, next) => {
+        console.groupCollapsed('路由前置守卫程序 ' + from.path + ' ---> ' + to.path)
+        const Path = usePathStore()
+        if (to.meta.needLogin) {
+          // 分支1: 访问非白名单路径，须先登录，否则重定向到登录页面
+          Logger.log('访问非白名单页面')
+          const appStore = useAppStore()
+          const authorityStore = useAuthorityStore()
+          if (Token.hasToken() && !Token.isExpired()) {
+            Logger.log('用户已登录，token未过期')
+            // TODO
+            if (to.meta.needPermission && !canAccessRoute(to.meta.permission)) {
+              Logger.log('用户无权限访问此非白名单页面，跳转到401页面')
+              next(Path.data.NOT_AUTHORIZED_EMBEDDED)
+              return
+            }
+            Logger.log('用户允许访问该非白名单页面')
+            next()
+          } else {
+            Logger.log('用户token不存在或已过期，重定向到登录页面')
+            if (!Token.hasToken()) {
+              ElMessage.error('用户未登录')
+            } else {
+              ElMessage.error('登录已过期，请重新登录')
+            }
+            appStore.initialize()
+            next(Path.data.LOGIN)
+          }
+        } else {
+          // 分支2: 访问白名单路径（不需要登录即可访问）
+          Logger.log('访问白名单页面')
+          if (
+            (to.path === Path.data.PORTAL || to.path === Path.data.LOGIN || to.path === Path.data.REGISTER || to.path === Path.data.RESET_PASSWORD) &&
+            Token.hasToken() &&
+            !Token.isExpired()
+          ) {
+            Logger.log('用户已登录，不允许访问门户页面，重定向到主页')
+            next(Path.data.INDEX)
+          } else {
+            // 其他非门户的白名单页面不论登录与否均可访问
+            next()
+          }
+        }
       })
       return router.value
     }
@@ -90,57 +140,3 @@ export const useRouterStore = defineStore('router', () => {
 //   })
 //   return result
 // }
-
-/**
- * 路由前置守卫：每次路由切换时先执行的操作
- * @param {*} to   要到哪里去
- * @param {*} from 从哪里来
- * @param {*} next 是否要去？
- */
-// router.beforeEach((to, from, next) => {
-//   console.groupCollapsed('路由前置守卫程序 ' + from.path + ' ---> ' + to.path)
-//   if (noLoginRequired.indexOf(to.path) > -1) {
-//     // ----- 1、访问白名单路径（不需要登录即可访问的路径） -----
-//     console.log('访问白名单页面')
-//     if ((to.path === Routes.PORTAL || to.path === Routes.LOGIN || to.path === Routes.REGISTER) && Token.hasToken() && !Token.isExpired()) {
-//       console.log('用户已登录，不允许访问登录和注册页面，重定向到主页')
-//       console.groupEnd()
-//       next(Routes.INDEX)
-//     } else {
-//       // 其他的白名单页面不论登录与否均可访问
-//       console.groupEnd()
-//       next()
-//     }
-//   } else {
-//     // ----- 2、访问非白名单路径，须先登录，否则重定向到登录页面 -----
-//     console.log('访问非白名单页面')
-//     const appStore = useAppStore()
-//     const authorityStore = useAuthorityStore()
-//     if (Token.hasToken() && !Token.isExpired()) {
-//       console.log('用户已登录，token未过期，允许访问页面')
-//       if (noPermissionRequired.indexOf(to.path) < 0) {
-//         console.log('访问的页面需要检查用户权限')
-//         if (!authorityStore.exist || !checkRouteAccessPermission(to.name)) {
-//           console.log('用户无权限访问页面，跳转到401页面')
-//           console.groupEnd()
-//           next(Routes.NOT_AUTHORIZED_EMBEDDED)
-//           return
-//         }
-//         console.log('用户具备访问页面的权限')
-//       }
-//       // 访问的页面不需要检查用户权限
-//       console.groupEnd()
-//       next()
-//     } else {
-//       console.log('用户token不存在或已过期，重定向到登录页面')
-//       if (!Token.hasToken()) {
-//         ElMessage.error('用户未登录')
-//       } else {
-//         ElMessage.error('登录已过期，请重新登录')
-//       }
-//       appStore.initialize()
-//       console.groupEnd()
-//       next(Routes.LOGIN)
-//     }
-//   }
-// })
--- src/router/routeTable/index.js
+++ src/router/routeTable/index.js
@@ -1,7 +1,6 @@
-import portalRoutes from './modules/portal'
 import homepageRoutes from './modules/homepage'
 import ledgerRoutes from './modules/ledger'
 import iamRoutes from './modules/iam'
 import errorsRoutes from './modules/errors'
 
-export default [...portalRoutes, ...homepageRoutes, ...ledgerRoutes, ...iamRoutes, ...errorsRoutes]
+export default [...homepageRoutes, ...ledgerRoutes, ...iamRoutes, ...errorsRoutes]
