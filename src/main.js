83f9a40882735306694b6731646bc3d7c0b08b8a
WyattMac <wyatt6@163.com>
2025-10-12 23:06:16 +0800

    自定义指令

--- src/main.js
+++ src/main.js
@@ -2,7 +2,8 @@
 import ElementPlus from 'element-plus'
 import zhCN from 'element-plus/es/locale/lang/zh-cn'
 import 'element-plus/dist/index.css'
-import installDirectives from './plugins/directives' // 引入自定义指令
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+import installDirectives from './directives' // 引入自定义指令
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
@@ -12,5 +13,12 @@ const app = createApp(App)
 
 app.use(ElementPlus, { locale: zhCN })
 app.use(createPinia())
+
+// 全局注册ElementPlus提供的图标
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+  app.component(key, component)
+}
+// 全局注册自定义指令
 installDirectives(app)
+
 await useSystemStore().initialize(app)
