7dd1128b65e4b490c92399991a6a91f36b333fb6
WyattMac <wyatt6@163.com>
2025-10-12 21:29:56 +0800

    删除无用代码

--- src/router/routeTable/index.js
+++
@@ -1,6 +0,0 @@
-import homepageRoutes from './modules/homepage'
-import ledgerRoutes from './modules/ledger'
-import iamRoutes from './modules/iam'
-import errorsRoutes from './modules/errors'
-
-export default [...homepageRoutes, ...ledgerRoutes, ...iamRoutes, ...errorsRoutes]
