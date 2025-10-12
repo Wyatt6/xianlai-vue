93927d565d052004106e676f8644d011f0657487
WyattMac <wyatt6@163.com>
2025-10-12 19:42:09 +0800

    根据权限显示菜单

--- src/views/layout/components/Menubar/MenuItem.vue
+++ src/views/layout/components/Menubar/MenuItem.vue
@@ -1,27 +1,29 @@
 <template>
-  <el-sub-menu v-if="menu.children && menu.children.length > 0" :index="menu.path">
-    <template #title>
+  <div v-if="showMenu">
+    <el-sub-menu v-if="menu.children && menu.children.length > 0" :index="menu.path">
+      <template #title>
+        <div v-if="menu.icon" class="icon-wrap">
+          <el-icon v-if="menu.icon.includes('el-icon')" size="2rem">
+            <component :is="menu.icon.substring(8, menu.icon.length)" />
+          </el-icon>
+          <LocalIcon v-else class="custom-icon" :name="menu.icon" size="2rem" />
+        </div>
+        <span class="title">{{ menu.title }}</span>
+      </template>
+      <MenuItem v-for="item in menu.children" :key="item.path" :menu="item" />
+    </el-sub-menu>
+    <el-menu-item v-else :index="menu.path">
       <div v-if="menu.icon" class="icon-wrap">
         <el-icon v-if="menu.icon.includes('el-icon')" size="2rem">
           <component :is="menu.icon.substring(8, menu.icon.length)" />
         </el-icon>
         <LocalIcon v-else class="custom-icon" :name="menu.icon" size="2rem" />
       </div>
-      <span class="title">{{ menu.title }}</span>
-    </template>
-    <MenuItem v-for="item in menu.children" :key="item.path" :menu="item" />
-  </el-sub-menu>
-  <el-menu-item v-else :index="menu.path">
-    <div v-if="menu.icon" class="icon-wrap">
-      <el-icon v-if="menu.icon.includes('el-icon')" size="2rem">
-        <component :is="menu.icon.substring(8, menu.icon.length)" />
-      </el-icon>
-      <LocalIcon v-else class="custom-icon" :name="menu.icon" size="2rem" />
-    </div>
-    <template #title>
-      <span class="title">{{ menu.title }}</span>
-    </template>
-  </el-menu-item>
+      <template #title>
+        <span class="title">{{ menu.title }}</span>
+      </template>
+    </el-menu-item>
+  </div>
 </template>
 
 <script setup>
@@ -33,13 +35,23 @@
  * 注意<el-submenu>是旧版本^1.0.2-beta.28的标签了
  */
 import LocalIcon from '@/components/LocalIcon/index.vue'
-import { defineProps } from 'vue'
+import { computed } from 'vue'
 import MenuItem from './MenuItem.vue'
+import Storage from '@/utils/storage'
 
-defineProps({
+const props = defineProps({
   menu: {
     type: Object,
     required: true
+  }
+})
+
+const showMenu = computed(() => {
+  if (props.menu.needPermission) {
+    const permissions = Storage.get(Storage.keys.PERMISSIONS)
+    return permissions.indexOf(props.menu.permission) > -1
+  } else {
+    return true
   }
 })
 </script>
