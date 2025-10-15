c31cd71bd8c81a88ee2249fea2a716d5ca244c29
WyattMac <wyatt6@163.com>
2025-10-15 09:55:47 +0800

    固定住内容区页面，禁止被拖动

--- src/views/layout/components/Content/index.vue
+++ src/views/layout/components/Content/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="content-wrap">
+  <div class="content-wrap" :class="[Layout.menubarExpand ? '' : 'menubar-hidden-content-wrap']">
     <router-view v-slot="{ Component, route }">
       <!-- TODO 使用KeepAlive也无法缓存组件状态，每次路由切换同样需要加载，还会钩子函数执行混乱 -->
       <keep-alive>
@@ -20,6 +20,7 @@ import { useLayoutStore } from '@/stores/layout'
 import { hasText, notEmpty } from '@/utils/common'
 
 const route = useRoute()
+const Layout = useLayoutStore()
 
 /**
  * 判断是否应该显示为页面标签
@@ -50,10 +51,16 @@ $--padding-top: 0.5rem;
 $--padding-left-right: 0.7rem;
 
 .content-wrap {
-  width: 100%;
+  position: fixed;
+  width: calc(100% - vars.$menubar-width);
   height: 100%;
   padding: calc(vars.$navbar-height + vars.$tagbar-height + $--padding-top) $--padding-left-right 0 $--padding-left-right;
   overflow: hidden;
   box-sizing: border-box;
+  transition: width #{vars.$menubar-transition-duration};
+}
+
+.menubar-hidden-content-wrap {
+  width: calc(100% - vars.$menubar-width-hidden) !important;
 }
 </style>
