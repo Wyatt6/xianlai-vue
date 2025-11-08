3d57b8dd6d0885af1cb16da311db51b4b10b3f86
WyattLau <wyatt6@163.com>
2025-11-08 23:46:53 +0800

    Update EditRoute.vue

--- src/views/setting/route_manage/EditRoute.vue
+++ src/views/setting/route_manage/EditRoute.vue
@@ -72,7 +72,6 @@
 <script setup>
 import { ref, watch } from 'vue'
 import { ElMessage } from 'element-plus'
-import Logger from '@/utils/logger'
 import { useApiStore } from '@/apis'
 
 const Api = useApiStore()
@@ -89,17 +88,14 @@ const props = defineProps({
   },
   routes: {
     type: Array,
-    default: [],
     required: true
   },
   paths: {
     type: Array,
-    default: [],
     required: true
   },
   perms: {
     type: Array,
-    default: [],
     required: true
   }
 })
@@ -164,10 +160,10 @@ watch(
 
 // ----- 点击“确定” -----
 const onConfirm = () => {
-  Logger.log('编辑路由')
+  console.log('编辑路由')
   formRef.value.validate(async valid => {
     if (valid) {
-      Logger.log('通过表单格式验证')
+      console.log('通过表单格式验证')
       loading.value = true
       if (
         form.value.sortId == props.nowRow.sortId &&
@@ -183,7 +179,7 @@ const onConfirm = () => {
         form.value.showTag == props.nowRow.showTag &&
         form.value.tagTitle == props.nowRow.tagTitle
       ) {
-        Logger.log('路由无修改')
+        console.log('路由无修改')
         ElMessage.info('路由无修改')
         loading.value = false
         return
@@ -210,12 +206,12 @@ const onConfirm = () => {
         })
         .then(result => {
           if (result && result.success) {
-            Logger.log('编辑路由成功')
+            console.log('编辑路由成功')
             ElMessage.success('保存成功')
             onClose()
             emits('afterEdit', result.data.route) // 调用父组件afterEdit事件
           } else {
-            Logger.log('编辑路由失败')
+            console.log('编辑路由失败')
             ElMessage.error(result && result.data.failMessage ? result.data.failMessage : '编辑路由失败')
           }
         })
