0654cdb37de5ad6ebcf62cb83dd44e90b3b9aed0
WyattLau <wyatt6@163.com>
2025-11-08 23:31:46 +0800

    Update system.js

--- src/stores/system.js
+++ src/stores/system.js
@@ -2,8 +2,6 @@ import { defineStore } from 'pinia'
 import { ref } from 'vue'
 import axios from 'axios'
 import { notEmpty, hasText } from '@/utils/common'
-import Token from '@/utils/token'
-import Logger from '@/utils/logger'
 import { useApiStore } from '@/apis'
 import { useRouterStore } from '@/router'
 import { useOptionStore } from './option'
@@ -41,46 +39,43 @@ export const useSystemStore = defineStore('system', () => {
           if (result.success) {
             if (notEmpty(result.data) && notEmpty(result.data.checksum)) {
               initData.value = result.data
-              // 登陆过期时初始化缓存数据
-              if (Token.hasToken() && Token.isExpired()) {
-                Logger.log('登陆已过期')
-                Token.setExpiredTime(0)
-              }
               // 清除旧的路由实例
               await useRouterStore().clearRouter()
               // 系统参数
               if (notEmpty(result.data.options) && hasText(result.data.checksum.optionsChecksum)) {
                 await useOptionStore().evalData(result.data.options, result.data.checksum.optionsChecksum)
-                Logger.log('系统参数初始化完成')
+                console.log('参数数据加载完成')
               }
               // 系统路径
               if (notEmpty(result.data.paths) && hasText(result.data.checksum.pathsChecksum)) {
                 await usePathStore().evalData(result.data.paths, result.data.checksum.pathsChecksum)
-                Logger.log('系统路径初始化完成')
+                console.log('路径数据加载完成')
               }
               // 系统路由
               if (notEmpty(result.data.routes) && hasText(result.data.checksum.routesChecksum)) {
                 await useRouterStore().evalData(result.data.routes, result.data.checksum.routesChecksum)
-                Logger.log('系统路由初始化完成')
+                console.log('路由数据加载完成')
               }
               // 系统菜单
               if (notEmpty(result.data.menus) && hasText(result.data.checksum.menusChecksum)) {
                 await useMenuStore().evalData(result.data.menus, result.data.checksum.menusChecksum)
-                Logger.log('系统菜单初始化完成')
+                console.log('菜单数据加载完成')
               }
               // 注册router插件
               if (app != null) {
                 app.use(useRouterStore().getRouter())
+                console.log('router插件注册完成')
               }
-              // 系统接口
+              // 系统接口（要用到router插件）
               if (notEmpty(result.data.apis) && hasText(result.data.checksum.apisChecksum)) {
                 await useApiStore().evalData(result.data.apis, result.data.checksum.apisChecksum)
-                Logger.log('系统接口初始化完成')
+                console.log('接口数据加载完成')
               }
               // 挂载页面到Vue应用
-              Logger.log('系统初始化完成')
+              console.log('系统初始化完成')
               if (app != null) {
                 app.mount('#app')
+                console.log('#app已挂载')
               }
             }
           } else {
