074fac0fc59bd7415668ab9e0956b6fb189e9fbe
WyattLau <wyatt6@163.com>
2025-11-08 23:44:36 +0800

    Update AddApi.vue

--- src/views/setting/api_manage/AddApi.vue
+++ src/views/setting/api_manage/AddApi.vue
@@ -28,7 +28,6 @@
 import { ref, watch } from 'vue'
 import { ElMessage } from 'element-plus'
 import { useApiStore } from '@/apis'
-import Logger from '@/utils/logger'
 
 const Api = useApiStore()
 
@@ -74,10 +73,10 @@ watch(
  * 点击“确定”
  */
 function onConfirm() {
-  Logger.log('新增接口')
+  console.log('新增接口')
   formRef.value.validate(async valid => {
     if (valid) {
-      Logger.log('通过表单格式验证')
+      console.log('通过表单格式验证')
       loading.value = true
       await Api.request.common.api
         .add(null, form.value)
@@ -86,12 +85,12 @@ function onConfirm() {
         })
         .then(result => {
           if (result && result.success) {
-            Logger.log('新增接口成功')
+            console.log('新增接口成功')
             ElMessage.success('新增接口成功')
             onClose()
             emits('afterAdd', result.data.api, result.data.rowNum) // 调用父组件afterAdd事件
           } else {
-            Logger.log('新增接口失败')
+            console.log('新增接口失败')
             ElMessage.error(result && result.data.failMessage ? result.data.failMessage : '新增接口失败')
           }
         })
